8d0ee8bde0b625abd3160760e37bb6f69bfe9c3e
Louis Soto <louis.soto@etu.utc.fr>
2021-12-14 19:32:38 +0000

    Update constant.go for music

--- Boid/utils/constant/constant.go
+++ Boid/utils/constant/constant.go
@@ -1,16 +1,13 @@
 package constant
 
 const (
-	ScreenWidth                = 600
-	ScreenHeight               = 600
-	NumBoids                   = 100
-	NumWalls                   = 20
-	MaxForce                   = 2.0
-	MaxSpeed                   = 4.0
-	AlignPerception            = 75.0
-	CohesionPerception         = 300.0
-	SeparationPerception       = 50.0
-	WallSeparationPerception   = 25.0 //increase it and a wall will looks like a repulsive object
-	NumSpecies                 = 3
-	RepulsionFactorBtwnSpecies = 100
+	ScreenWidth              = 500
+	ScreenHeight             = 500
+	NumBoids                 = 100
+	NumWalls                 = 20
+	MaxForce                 = 2.0
+	MaxSpeed                 = 4.0
+	AlignPerception          = 75.0
+	WallSeparationPerception = 25.0 //increase it and the wall will looks like a repulsive object
+	NumSpecies               = 3
 )
