307e4a592ecca142a0deb095aa925e0e6160e5f7
Louis Soto <louis.soto@etu.utc.fr>
2021-12-14 15:01:07 +0000

    Téléverser un nouveau fichier

---
+++ Boid/utils/constant/constant.go
@@ -0,0 +1,16 @@
+package constant
+
+const (
+	ScreenWidth                = 600
+	ScreenHeight               = 600
+	NumBoids                   = 100
+	NumWalls                   = 20
+	MaxForce                   = 2.0
+	MaxSpeed                   = 4.0
+	AlignPerception            = 75.0
+	CohesionPerception         = 300.0
+	SeparationPerception       = 50.0
+	WallSeparationPerception   = 75.0
+	NumSpecies                 = 3
+	RepulsionFactorBtwnSpecies = 100
+)
