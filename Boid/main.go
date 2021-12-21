7c9aad4133e61d470a286337c8f54d6eaff45486
Vivien Leclercq <vivien.leclercq@etu.utc.fr>
2021-12-21 17:34:17 +0000

    Update main.go

--- Boid/main.go
+++ Boid/main.go
@@ -1,32 +1,67 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"log"
+
+	ebiten "github.com/hajimehoshi/ebiten/v2"
+	ebitenutil "github.com/hajimehoshi/ebiten/v2/ebitenutil"
+
 	music "gitlab.utc.fr/projet_ia04/Boid/agent/music"
 	game "gitlab.utc.fr/projet_ia04/Boid/game"
 	constant "gitlab.utc.fr/projet_ia04/Boid/utils/constant"
 	variable "gitlab.utc.fr/projet_ia04/Boid/utils/variable"
-	"log"
 )
 
 func init() {
-	fish, _, err := ebitenutil.NewImageFromFile("utils/fish/chevron-up.png", 0)
+	fish, _, err := ebitenutil.NewImageFromFile("utils/fish/chevron-up.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 	w, h := fish.Size()
-	variable.BirdImage, _ = ebiten.NewImage(w-20, h-10, 0)
-	variable.WallImage, _ = ebiten.NewImage(w, h, 0)
+	variable.BirdImage = ebiten.NewImage(w-20, h-10)
+	variable.WallImage = ebiten.NewImage(w, h)
 	op := &ebiten.DrawImageOptions{}
 	variable.BirdImage.DrawImage(fish, op)
 	variable.WallImage.DrawImage(fish, op)
+
+	fish1, _, err := ebitenutil.NewImageFromFile("utils/fish/poisson-2.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w, h = fish1.Size()
+	variable.FishImage1 = ebiten.NewImage(w, h)
+	variable.FishImage1.DrawImage(fish1, op)
+
+	fish2, _, err := ebitenutil.NewImageFromFile("utils/fish/poisson-3.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w, h = fish2.Size()
+	variable.FishImage2 = ebiten.NewImage(w, h)
+	variable.FishImage2.DrawImage(fish2, op)
+
+	fish3, _, err := ebitenutil.NewImageFromFile("utils/fish/poisson-5.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w, h = fish1.Size()
+	variable.FishImage3 = ebiten.NewImage(w, h)
+	variable.FishImage3.DrawImage(fish3, op)
+
+	back, _, err := ebitenutil.NewImageFromFile("utils/fish/background.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w, h = back.Size()
+	variable.BackgroundImage = ebiten.NewImage(w, h)
+	variable.BackgroundImage.DrawImage(back, op)
+
 }
 
 func main() {
 	// création de la chanel de sync pour la music:
 	c := make(chan string)
-	musicAgent := music.NewMusicAgent("utils/music/virtual-riot-the-darkest-night.mp3", "utils/music/virtual-riot-the-darkest-night.wav", c)
+	musicAgent := music.NewMusicAgent("utils/music/jaws.mp3", "utils/music/virtual-riot-the-darkest-night.wav", c)
 	musicAgent.Start()
 
 	ebiten.SetWindowSize(constant.ScreenWidth, constant.ScreenHeight)
