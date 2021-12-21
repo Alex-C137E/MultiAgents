47c09928f6229961cc8319bcfac3afdf7983a9b3
Vivien Leclercq <vivien.leclercq@etu.utc.fr>
2021-12-21 17:35:58 +0000

    Update boid.go

--- Boid/agent/boid/boid.go
+++ Boid/agent/boid/boid.go
@@ -16,72 +16,77 @@ type Boid struct {
 	Velocity     Vector2D
 	Acceleration Vector2D
 	Species      int
+	Dead         bool
 }
 
 func (boid *Boid) ApplyRules(restOfFlock []*Boid) {
-	alignSteering := Vector2D{}
-	alignTotal := 0
-	cohesionSteering := Vector2D{}
-	cohesionTotal := 0
-	separationSteering := Vector2D{}
-	separationTotal := 0
+	if !boid.Dead {
+		alignSteering := Vector2D{}
+		alignTotal := 0
+		cohesionSteering := Vector2D{}
+		cohesionTotal := 0
+		separationSteering := Vector2D{}
+		separationTotal := 0
 
-	for _, other := range restOfFlock {
-		d := boid.Position.Distance(other.Position)
-		if boid != other {
-			if boid.Species == other.Species && d < constant.AlignPerception {
-				alignTotal++
-				alignSteering.Add(other.Velocity)
-			}
-			if boid.Species == other.Species && d < variable.CohesionPerception {
-				cohesionTotal++
-				cohesionSteering.Add(other.Position)
-			}
-			if d < variable.SeparationPerception {
-				separationTotal++
-				diff := boid.Position
-				diff.Subtract(other.Position)
-				diff.Divide(d)
-				separationSteering.Add(diff)
-				if other.Species != boid.Species {
-					diff.Divide(d * variable.RepulsionFactorBtwnSpecies)
+		for _, other := range restOfFlock {
+			d := boid.Position.Distance(other.Position)
+			if boid != other {
+				if boid.Species == other.Species && d < constant.AlignPerception {
+					alignTotal++
+					alignSteering.Add(other.Velocity)
+				}
+				if boid.Species == other.Species && d < variable.CohesionPerception {
+					cohesionTotal++
+					cohesionSteering.Add(other.Position)
+				}
+				if d < variable.SeparationPerception {
+					separationTotal++
+					diff := boid.Position
+					diff.Subtract(other.Position)
+					diff.Divide(d)
 					separationSteering.Add(diff)
+					if other.Species != boid.Species {
+						diff.Divide(d * variable.RepulsionFactorBtwnSpecies)
+						separationSteering.Add(diff)
+					}
 				}
 			}
 		}
-	}
 
-	if separationTotal > 0 {
-		separationSteering.Divide(float64(separationTotal))
-		separationSteering.SetMagnitude(constant.MaxSpeed)
-		separationSteering.Subtract(boid.Velocity)
-		separationSteering.SetMagnitude(constant.MaxForce * 1.2)
-	}
-	if cohesionTotal > 0 {
-		cohesionSteering.Divide(float64(cohesionTotal))
-		cohesionSteering.Subtract(boid.Position)
-		cohesionSteering.SetMagnitude(constant.MaxSpeed)
-		cohesionSteering.Subtract(boid.Velocity)
-		cohesionSteering.SetMagnitude(constant.MaxForce * 0.9)
-	}
-	if alignTotal > 0 {
-		alignSteering.Divide(float64(alignTotal))
-		alignSteering.SetMagnitude(constant.MaxSpeed)
-		alignSteering.Subtract(boid.Velocity)
-		alignSteering.Limit(constant.MaxForce)
-	}
+		if separationTotal > 0 {
+			separationSteering.Divide(float64(separationTotal))
+			separationSteering.SetMagnitude(constant.MaxSpeed)
+			separationSteering.Subtract(boid.Velocity)
+			separationSteering.SetMagnitude(constant.MaxForce * 1.2)
+		}
+		if cohesionTotal > 0 {
+			cohesionSteering.Divide(float64(cohesionTotal))
+			cohesionSteering.Subtract(boid.Position)
+			cohesionSteering.SetMagnitude(constant.MaxSpeed)
+			cohesionSteering.Subtract(boid.Velocity)
+			cohesionSteering.SetMagnitude(constant.MaxForce * 0.9)
+		}
+		if alignTotal > 0 {
+			alignSteering.Divide(float64(alignTotal))
+			alignSteering.SetMagnitude(constant.MaxSpeed)
+			alignSteering.Subtract(boid.Velocity)
+			alignSteering.Limit(constant.MaxForce)
+		}
 
-	boid.Acceleration.Add(alignSteering)
-	boid.Acceleration.Add(cohesionSteering)
-	boid.Acceleration.Add(separationSteering)
-	boid.Acceleration.Divide(3)
+		boid.Acceleration.Add(alignSteering)
+		boid.Acceleration.Add(cohesionSteering)
+		boid.Acceleration.Add(separationSteering)
+		boid.Acceleration.Divide(3)
+	}
 }
 
 func (boid *Boid) ApplyMovement() {
-	boid.Position.Add(boid.Velocity)
-	boid.Velocity.Add(boid.Acceleration)
-	boid.Velocity.Limit(constant.MaxSpeed)
-	boid.Acceleration.Multiply(0.0)
+	if !boid.Dead {
+		boid.Position.Add(boid.Velocity)
+		boid.Velocity.Add(boid.Acceleration)
+		boid.Velocity.Limit(constant.MaxSpeed)
+		boid.Acceleration.Multiply(0.0)
+	}
 }
 
 // func (boid *Boid) CheckEdges() bool {
