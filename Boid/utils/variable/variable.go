7eda9839f7a086830188c4bf5962299dc6bae7d7
Vivien Leclercq <vivien.leclercq@etu.utc.fr>
2021-12-21 17:38:37 +0000

    Update variable.go

--- Boid/utils/variable/variable.go
+++ Boid/utils/variable/variable.go
@@ -1,11 +1,15 @@
 package variable
 
 import (
-	"github.com/hajimehoshi/ebiten"
+	ebiten "github.com/hajimehoshi/ebiten/v2"
 )
 
 var (
 	BirdImage                  *ebiten.Image
+	FishImage1                 *ebiten.Image
+	FishImage2                 *ebiten.Image
+	FishImage3                 *ebiten.Image
+	BackgroundImage            *ebiten.Image
 	WallImage                  *ebiten.Image
 	RepulsionFactorBtwnSpecies float64
 	SeparationPerception       float64
