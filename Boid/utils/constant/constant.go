c4ad67ac1771cf03fee10fa24119d04b6868cb1e
Vivien Leclercq <vivien.leclercq@etu.utc.fr>
2021-12-21 17:38:20 +0000

    Update constant.go

--- Boid/utils/constant/constant.go
+++ Boid/utils/constant/constant.go
@@ -1,9 +1,9 @@
 package constant
 
 const (
-	ScreenWidth              = 500
-	ScreenHeight             = 500
-	NumBoids                 = 100
+	ScreenWidth              = 1152
+	ScreenHeight             = 648
+	NumBoids                 = 200
 	NumWalls                 = 20
 	MaxForce                 = 2.0
 	MaxSpeed                 = 4.0
