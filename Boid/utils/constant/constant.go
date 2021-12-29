1753d18fcef98e6e11ff0136d6366e0e5328414d
Louis Soto <louis.soto@etu.utc.fr>
2021-12-29 00:07:29 +0000

    levels

--- Boid/utils/constant/constant.go
+++ Boid/utils/constant/constant.go
@@ -3,12 +3,8 @@ package constant
 const (
 	ScreenWidth              = 1152
 	ScreenHeight             = 648
-	NumBoids                 = 300
+	NumBoids                 = 200
 	NumPreda                 = 3
-	NumWalls                 = 30
-	MaxForce                 = 2.0
-	MaxSpeed                 = 4.0
-	AlignPerception          = 75.0
 	WallSeparationPerception = 25.0 //increase it and the wall will looks like a repulsive object
 	NumSpecies               = 3
 )
