af41686182c8caed5498088d15cef9cb231cccf0
Louis Soto <louis.soto@etu.utc.fr>
2021-12-31 00:27:33 +0000

    correctif levels

--- Boid/flock/flock.go
+++ Boid/flock/flock.go
@@ -3,6 +3,7 @@ package flock
 import (
 	boid "gitlab.utc.fr/projet_ia04/Boid/agent/boid"
 	wall "gitlab.utc.fr/projet_ia04/Boid/agent/wall"
+	"gitlab.utc.fr/projet_ia04/Boid/utils/constant"
 )
 
 type Flock struct {
@@ -11,7 +12,7 @@ type Flock struct {
 	Predators []*boid.Predator
 }
 
-func (flock *Flock) Logic() {
+func (flock *Flock) Logic(level int) {
 	for _, boid := range flock.Boids {
 		if !boid.CheckEdges() {
 			if !boid.CheckWalls(flock.Walls) {
@@ -19,6 +20,15 @@ func (flock *Flock) Logic() {
 			}
 		}
 		boid.ApplyMovement()
+
+		// Pour éviter que les poissons réussissent à s'échapper des murs de bombes
+		// dans les niveaux supérieurs ou égal au niveau 4: (le dernier niveau pour le moment)
+		// Dès que l'on detecte qu'ils ne sont pas où ils devraient être, on les fait réapparaitre au centre
+
+		if level >= 4 && (boid.Position.Y <= 0 || boid.Position.Y >= float64(constant.ScreenHeight)) {
+			boid.Position.Y = float64(constant.ScreenHeight) / 2
+		}
+
 	}
 	for _, preda := range flock.Predators {
 		if !preda.CheckEdges() {
