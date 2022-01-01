5bb509550ce3c62b02ff147821d3f154eca5350c
Louis Soto <louis.soto@etu.utc.fr>
2022-01-01 20:32:46 +0000

    Commentaires sure les niveaux améliorés

--- Boid/game/game.go
+++ Boid/game/game.go
@@ -57,31 +57,30 @@ func NewGame(c chan string, timeOut int) *Game {
 	//Création de 5 niveaux:
 	g.levels = make([]*Level, 5)
 
-	// niveau 0
-	// niveau très simple dans lequel les poisons sont amené à se regroupé et se stabilisé rapidement
+	// niveau 0: ce niveau est très simple:les poissons sont amenés à se regrouper et se stabiliser rapidement
+	// rendant leur pêche facile.
 	g.levels[0] = NewLevel(10000, 10, 300, 100, 16+10, 1, 4.0, 1000, 15)
 
-	// niveau 1
-	// premier niveau mais cette fois-ci les requin attaquent bien plus, il faut don  être plus rapide
+	// niveau 1:  ce niveau est identique au niveau 0 à la différence que cette fois-ci les requins attaquent bien 
+	// plus: leur attribue density est diminué de tel sorte à ce qu’il attaque pour une quantité de poisson dans leur 
+	// champ d’attaque inférieur à celle du niveau 0. Le joueur doit donc être plus rapide pour ne pas se faire 
+	// manger tous ses poisons par les prédateurs.
 	g.levels[1] = NewLevel(10000, 10, 300, 100, 16+10, 1, 4.0, 1000, 8)
 
-	// niveau 2
-	// même type de niveau que le 0, mais le regroupement prend plus de temps et la stabilité est moindre
-	// et le lasso est plus petit
+	// niveau 2:  ce niveau est dans la continuité du niveau 1 mais ici: le facteur de cohésion diminue, et ceux de 
+	// répulsion intra et inter espèces augmentent ce qui diminue la stabilité dans le comportement des poissons les 
+	// rendant plus complexes à attraper: ils se regroupent moins, et se mélangent plus entre espèces. De plus, la 
+	// taille maximale du filet diminue.
 	g.levels[2] = NewLevel(500, 100, 100, 75, 16+10, 2.0, 4.0, 700, 8)
 
-	// niveau 3
-	// on dimunie le facteur de repulsion entre espèce ainsi que celui de cohésion
-	// pour rendre plus dificile le fait de n'attrapper
-	// qu'une espèce
-	// et bien sur on adapte les requins en diminuant leur parametre densité
+	// niveau 3:  pour ce niveau, on reprend le niveau 2 et on augmente le niveau difficulté en réduisant le niveau 
+	// de stabilité de manière similaire à ce qui fut fait pour le niveau 2. En plus, on rend les prédateurs plus 
+	// agressifs en utilisant le même procédé utilisé dans le niveau 1.
 	g.levels[3] = NewLevel(50, 100, 75, 75, 16+10, 2.0, 4.0, 700, 5)
 
-	// niveau 4
-	// on rajoute des mures/bombes: ce qui favorise le chaos et rend plus dificile la tâche
-	// d'attraper les poissons
-	// De plus les poissons sont moins en cohésion et vont plus vite
-	// en plus on améliore les requins :)
+	// niveau 4: pour ce niveau on reprend le niveau 3 et on  rajoute des murs/ bombes pour favoriser le chaos et 
+	// rendre plus difficile la tâche d’'attraper les poissons. De plus les poissons sont moins en cohésion et vont  
+	// plus vite et bien sûr, les prédateurs sont encore plus agressifs :).
 	g.levels[4] = NewLevel(50, 100, 50, 75, 16+10+2*48, 2.0, 5.0, 700, 3)
 
 	go func() {
